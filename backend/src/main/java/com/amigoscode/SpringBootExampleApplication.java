c39e2829cda5078239e11b98d8b9ee9e4fbcc16e
gthtawfiq <88319838+gthtawfiq@users.noreply.github.com>
2023-06-09 19:25:49 +0200

    move docker compose and pin , postgres to tag 15.3 , removing @RestController from SpringBootExampleApplication.java

--- backend/src/main/java/com/amigoscode/SpringBootExampleApplication.java
+++ backend/src/main/java/com/amigoscode/SpringBootExampleApplication.java
@@ -7,12 +7,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.RestController;
+
 
 import java.util.List;
 
 @SpringBootApplication
-@RestController
 public class SpringBootExampleApplication {
 	
 	//db for now
