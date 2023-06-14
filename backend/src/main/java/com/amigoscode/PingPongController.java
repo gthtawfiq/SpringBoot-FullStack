bf7387e4b3f34714798b767c674fc710194a4a76
gthtawfiq <88319838+gthtawfiq@users.noreply.github.com>
2023-06-14 12:57:15 +0200

    CD Workflow with new controller

---
+++ backend/src/main/java/com/amigoscode/PingPongController.java
@@ -0,0 +1,14 @@
+package com.amigoscode;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingPongController {
+    record Pingpong(String result){}
+
+    @GetMapping("/ping")
+    public Pingpong getPingpong(){
+        return new Pingpong("Pong");
+    }
+}
