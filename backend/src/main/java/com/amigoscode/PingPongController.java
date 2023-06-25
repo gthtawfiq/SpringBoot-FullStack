9d2dead38eb9aed21d710d6ff6573cf70f2429ed
gthtawfiq <88319838+gthtawfiq@users.noreply.github.com>
2023-06-25 13:29:48 +0200

    add counter to PingPongController

--- backend/src/main/java/com/amigoscode/PingPongController.java
+++ backend/src/main/java/com/amigoscode/PingPongController.java
@@ -5,11 +5,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class PingPongController {
+    private static int counter=0;
     record Pingpong(String result){}
 
     @GetMapping("/ping")
     public Pingpong getPingpong(){
-        return new Pingpong("Pong");
+        return new Pingpong("Pong "+ ++counter);// ++counter this to start with 1 at first then it will follow with 2,3,4...etc
+                                                     // if we use counter++ the value of counter will start with 0 at first then increment with 1,2,3...etc
     }
 
 
